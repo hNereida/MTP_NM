2b7a5f260092e7f5e6d1f9209ee9eca4e11fc6e4
Miquellaneras <50173851+Miquellaneras@users.noreply.github.com>
2021-11-27 20:20:42 +0100

    Add files via upload

--- Functions_NM.py
+++ Functions_NM.py
@@ -1,6 +1,6 @@
-import random
 import time
 import os
+import math
 import RF24
 
 # CONSTANTS 
@@ -14,16 +14,35 @@ from Packets.DataPacket import DataPacket
 from Packets.DataPacketResponse import DataPacketResponse
 from Packets.TokenPacket import TokenPacket
 from Packets.TokenPacketResponse import TokenPacketResponse
+from Packets.PacketGeneric import PacketGeneric
+
+# Variables
+
+radio = None
 
 # FUNCIONS AUXILIARS (ES POSARAN EN UN ALTRE DOCUMENT)
+
+def initialize_radio(): # quan es para la radio?
+    global radio
+    radio = RF24.RF24()
+    radio.begin(25,0)
+    radio.setDataRate(CNTS.DATARATE)
+    radio.setChannel(CNTS.CHANNEL) 
+    radio.setRetries(1, CNTS.RETRIES)
+    radio.setPALevel(CNTS.POWERLEVEL)
+    radio.openWritingPipe(CNTS.PIPES)
+    radio.openReadingPipe(1, CNTS.PIPES)
+    radio.powerUp()
+    return radio
+
 
 def is_usb_connected():
     usbpath = "/media/pi/" # Modificar per cada raspberry
     isconnected = False
-    time.sleep(5)
+    time.sleep(0.01)
     if len(os.listdir(os.path.dirname(usbpath))) != 0:
         usb = os.listdir(os.path.dirname(usbpath))[0]
-        time.sleep(5)
+        time.sleep(0.01)
         files = os.listdir(os.path.join(usbpath, usb))
         if len(files) != 0:
             isconnected = True
@@ -42,28 +61,24 @@ def read_usb_file():
 # Fer import de radio, mirar el self
 # MIRAR COM S'ADAPTA AMB LES FUNCIONS DEL TEAM B
 # UTILITZAR LES CONSTANTS DE RETRIES I TIMEOUTS
-def send_hello(source_address, dest_address):
+def send_hello(srcAddress, rcvAddress):
     responded = False
     hasData = False
     hadToken = False
-    radio = RF24.RF24()
-    radio.stopListening()
-    # Part Team B
-    hello_packet = HelloPacket(source_address, dest_address)
-    packetToSend = hello_packet.buildPacket()
-    radio.write(packetToSend)
-    # Part Team B
-    # Esperar un temps??? --> GP: not sure
-    time.sleep(0.1)
-
-     # buscar funcio per mirar si hi ha dades rebudes IMPORTANT!!! (potser es aquesta)
-    radio.startListening()
-    while not radio.available():
-      time.sleep(CNTS.TIMEOUT)
-
-    # IMPLEMENTAR TIMEOUTS I RETRIES???
-    responded = True
-    rcvBytes = radio.read(radio.payloadSize)
+    helloPacket = HelloPacket(srcAddress, rcvAddress)
+    packetToSend = helloPacket.buildPacket()
+    
+    retries = 0
+    while retries < CNTS.RETRIES and not responded:
+        radio.stopListening()
+        radio.write(packetToSend)
+        radio.startListening()
+        time.sleep(CNTS.TIMEOUT)
+        if radio.available():
+            responded = True
+        retries += 1
+    
+    rcvBytes = radio.read(CNTS.PACKET_SIZE)
     rcvPacket = HelloPacketResponse()
     rcvPacket.parsePacket(rcvBytes)
     if rcvPacket.getTypePacket() == packets.HELLO_RESPONSE["type"]:
@@ -71,55 +86,80 @@ def send_hello(source_address, dest_address):
         hadToken = rcvPacket.hadToken()
     return responded, hasData, hadToken
 
+
+def send_hello_response(srcAddress, rcvAddress, haveData, hadToken):
+    helloPacketResponse = HelloPacketResponse(srcAddress, rcvAddress, haveData, hadToken)
+    packetToSend = helloPacketResponse.buildPacket()
+    radio.stopListening()
+    radio.write(packetToSend)
+
+
 # Declarar EOT
 # Retorna true si la data s'ha enviat correctament
 # MIRAR COM S'ADAPTA MAB LES FUNCIONS DEL TEAM B
 # ACABAR FUNCIO
-def send_data(myAddress, toAddress, data):
-    radio = RF24.RF24()
-    radio.begin(25,0) #set CE and IRQ pins
-    radio.setDataRate(CNTS.DATARATE) 
-    radio.setChannel(CNTS.CHANNEL) #Set Channel 6
-    radio.setRetries(1,CNTS.RETRIES) #250us*i , 4 retries - es pot canviar
-    radio.setPALevel(CNTS.POWERLEVEL) #0 dBm power amplifier level - see table in docu https://nrf24.github.io/RF24/classRF24.html
-    radio.openWritingPipe(bytearray(CNTS.PIPES))
-    radio.powerUp()
-    pack=0
-    #FUNCIO GRUP B createDataPacket(myAddress,toAddress,data)
-    identifier = CNTS.DATA_PACKET #identifier Data packet
-    for temp in to_send:
-        for x in range(0,len(temp),31):
-            data = identifier + pack
-            aux = bytearray(data.to_bytes(1,'big')) + temp[x:x+31]
-            while not(radio.write(aux)):    
-                print("failed delivery ")
-        pack+=1
-    while not(radio.write(EOT)):   
-        print("failed delivery " )
-        time.sleep(0.01)
-        return False
+def send_data(srcAddress, rcvAddress, fileData):
+    sentPackets = 0
+    EOF = False
+    sequenceNumber = False
+    packetSize = CNTS.DATA_SIZE
+    for x in range(0, len(fileData), 30):
+        sentPackets += 1
+        if sentPackets == math.ceil(len(fileData)/30):
+            packetSize = len(fileData) - (sentPackets-1)*30
+            EOF = True
+        dataPacket = DataPacket(srcAddress, rcvAddress, packetSize, EOF, sequenceNumber, fileData[x:x+packetSize])
+
+        responded = False
+        retries = 0
+        while retries <= CNTS.RETRIES and not responded:
+            radio.stopListening()
+            radio.write(dataPacket)
+            radio.startListening()
+            time.sleep(CNTS.TIMEOUT)
+            if radio.available():
+                rcvBytes = radio.read(CNTS.PACKET_SIZE)
+                rcvPacket = DataPacketResponse()
+                rcvPacket.parsePacket(rcvBytes)
+                # Check if it is the right packet type
+                if sequenceNumber == rcvPacket.getSequenceNumber() and rcvPacket.isValid():
+                    responded = True
+                else:
+                    retries += 1
+            else:
+                retries += 1
+            
+        sequenceNumber = not sequenceNumber
+
+        if retries > CNTS.RETRIES and not responded:
+            return False
+    
     return True
 
-def send_token(sourceAddress, toAddress, numRecvData):
-    radio = RF24.RF24()
-    radio.begin(25,0) #set CE and IRQ pins
-    radio.setDataRate(RF24.RF24_1MBPS) 
-    radio.setChannel(0x4c) #Set Channel 76
-    radio.setRetries(1,4) #250us*i , 4 retries - es pot canviar
-    radio.setPALevel(RF24.RF24_PA_MAX) #0 dBm power amplifier level - see table in docu https://nrf24.github.io/RF24/classRF24.html
-    radio.openWritingPipe(bytearray(CNTS.PIPES))
-    radio.powerUp()
-    pack=0
+def send_token(srcAddress, rcvAddress, token):
+    responded = False
+    tokenPacket = TokenPacket(srcAddress, rcvAddress, token)
+    packetToSend = tokenPacket.buildPacket()
+    
+    retries = 0
+    while retries < CNTS.RETRIES and not responded:
+        radio.stopListening()
+        radio.write(packetToSend)
+        radio.startListening()
+        time.sleep(CNTS.TIMEOUT)
+        if radio.available():
+            rcvBytes = radio.read(CNTS.PACKET_SIZE)
+            rcvPacket = TokenPacketResponse()
+            rcvPacket.parsePacket(rcvBytes)
+            # Check if it is the right packet type
+            if rcvPacket.isValid():
+                responded = True
+            else:
+                retries += 1
+        else:
+            retries += 1    
 
-    #FUNCIO GRUP B createTokenPacket(toAddress,token)
-    tokenPacket = TokenPacket(sourceAddress, toAddress, numRecvData)
-    token = tokenPacket.buildPacket()
-
-    while not(radio.write(token)):    
-      print("failed delivery ")
-      #return False
-
-    return True
+    return responded
 
 # Fer import de radio
 # Declarar EOT
@@ -127,91 +167,60 @@ def send_token(sourceAddress, toAddress, numRecvData):
 # ACABAR FUNCIO
 def wait_read_packets():
     finalData = ""
-    radio.openReadingPipe(1, pipesbytes)
     radio.startListening()
 
-    
     # Group B
     # Read one packet
     while not radio.available():
         time.sleep(0.01)
-    receivedPacket = radio.read(32)
+    rcvBytes = radio.read(CNTS.PACKET_SIZE)
 
-    if PacketGeneric.isPacket(receivedPacket, packets.HELLO["type"]):
+    if PacketGeneric.isPacket(rcvBytes, packets.HELLO["type"]):
         helloPacket = HelloPacket()
-        helloPacket.parsePacket(receivedPacket)
-        # do whatever with the packet
-
-    elif PacketGeneric.isPacket(receivedPacket, packets.HELLO_RESPONSE["type"]):
-        helloResponsePacket = HelloResponsePacket()
-        helloResponsePacket.parsePacket(receivedPacket)
-        # do whatever with the packet
+        helloPacket.parsePacket(rcvBytes)
+        return packets.HELLO["type"], helloPacket.getSourceAddress()
 
     # TODO: Check sequence number for Stop & Wait
-    elif PacketGeneric.isPacket(receivedPacket, packets.DATA["type"]):
+    if PacketGeneric.isPacket(rcvBytes, packets.DATA["type"]):
         dataPacket = DataPacket()
-        dataPacket.parsePacket(receivedPacket)
+        dataPacket.parsePacket(rcvBytes)
         finalData = dataPacket.getPayload()
 
-    while not dataPacket.isEot():
-        while not radio.available():
-            time.sleep(0.01)
-        receivedPacket = radio.read(32)
-        dataPacket.parsePacket(receivedPacket)
-        finalData.extend(dataPacket.getPayload())
+        # SORTIR DEL WHILE QUANT NO ES DATA
+        sequenceNumber = False
+        while not dataPacket.isEot():
+            while not radio.available():
+                time.sleep(0.01)
+            receivedPacket = radio.read(CNTS.PACKET_SIZE)
+            dataPacket.parsePacket(receivedPacket)
+            # Check CRC
+            if dataPacket.getSequenceNumber == sequenceNumber:
+                dataPacketResponse = DataPacketResponse(dataPacket.getDestinationAddress, dataPacket.getSourceAddress, sequenceNumber, True)
+            else:
+                dataPacketResponse = DataPacketResponse(dataPacket.getDestinationAddress, dataPacket.getSourceAddress, sequenceNumber, False)
+            packetToSend = dataPacketResponse.buildPacket()
+            radio.stopListening()
+            radio.write(packetToSend)
+            radio.startListening()
 
-    # TODO: Check sequence number for Stop & Wait
-    elif PacketGeneric.isPacket(receivedPacket, packets.DATA_RESPONSE["type"]):
-        dataResponsePacket = DataResponsePacket()
-        dataResponsePacket.parsePacket(receivedPacket)
-        # do whatever with the packet
+            finalData.extend(dataPacket.getPayload())
 
-    elif PacketGeneric.isPacket(receivedPacket, packets.TOKEN["type"]):
+        return packets.DATA["type"], finalData
+
+    if PacketGeneric.isPacket(rcvBytes, packets.TOKEN["type"]):
         tokenPacket = TokenPacket()
-        tokenPacket.parsePacket(receivedPacket)
-        # do whatever with the packet
+        tokenPacket.parsePacket(rcvBytes)
+        # We should check with the CRC that the packet is okey, value True of below
+        tokenPacketResponse = TokenPacketResponse(tokenPacket.getDestinationAddress, tokenPacket.getSourceAddress, True)
+        packetToSend = tokenPacketResponse.buildPacket()
+        radio.stopListening()
+        radio.write(packetToSend)
 
-    elif PacketGeneric.isPacket(receivedPacket, packets.TOKEN_RESPONSE["type"]):
-        tokenResponsePacket = TokenResponsePacket()
-        tokenResponsePacket.parsePacket(receivedPacket)
-    # do whatever with the packet
+        return packets.TOKEN["type"], tokenPacket.getNumRecvData()
 
-    """
-    while EOT not in receivedPacket:
-        while not radio.available():
-          time.sleep(0.01)
 
-        receivedPacket = radio.read(32)
-        header = receivedPacket[0]
-        #Multiplico la header per 00001110 per quedarme amb els bits on hi ha la info del packet
-        if (header&0x0E) == 0x00:
-            return finalData, CNTS.HELLO_PACKET
-        elif (header&0x0E) == 0x02:
-            return finalData, CNTS.HELLO_RESPONSE
-        elif (header&0x0E) == 0x04: 
-            seqReceived = bool(header & 0x01)
-            dec = header>>1
-            if EOT not in receivedPacket:
-                if seq == seqReceived:
-                    received[dec]=received[dec] + receivedPacket[1:32] 
-                    seq=not seq    
-        elif (header&0x0E) == 0x06:
-            return finalData, CNTS.DATA_ACK
-        elif (header&0x0E) == 0x08:
-            return finalData, CNTS.TOKEN_PACKET
-        elif (header&0x0E) == 0x0A:
-            return finalData, CNTS.TOKEN_ACK 
-    for dataReceived in received:
-        finalData.extend(dataReceived)
-    return finalData, CNTS.DATA_PACKET
-"""
 # CANVIAR A GUARDAR A RASPBERRY
 # ACABAR LA FUNCIO
 def write_file(data):
     with open("/mnt/USBDrive/fileOutput.txt","wb") as f:
         f.write(data)
-
-# #Functions to do:
-# int read_token()
-# send_token(address, token)
-# send_token_ack() # no se si cal aquesta funcio
--- State_Machine_NM.py
+++ State_Machine_NM.py
@@ -9,6 +9,8 @@ import Constants_NM as CNTS
 # FUNCIONS AUXILIARS
 import Functions_NM as Functions
 
+import Packets.PacketsDefinitions as packets
+
 # VARIABLES
 myAddress = 1
 
@@ -17,7 +19,7 @@ hadToken = False
 
 packetType = "111"
 fileData = bytearray()
-rxData = bytearray()
+rcvData = bytearray()
 
 # nodes = { adress: valor, hasData: True or False, hasToken: True or False, toSend: True or False}
 nodes = [{"adress": 2, "hasData": False, "hasToken": False, "toSendData": False},
@@ -29,13 +31,14 @@ nodes = [{"adress": 2, "hasData": False, "hasToken": False, "toSendData": False}
 token = 1
 
 lastNodeNoToken = 0
-nodesToSend = []
+nodesToSendToken = []
 
 # AQUI COMENCEN ELS ESTATS
 
 #Check if we have the USB connected. If we have it connected, we are the first to transmit. If not, we just wait.
 def s0():
     global fileData
+    Functions.initialize_radio()
     if Functions.is_usb_connected():
     	fileData = Functions.read_usb_file()
     	return s1()
@@ -45,10 +48,17 @@ def s0():
 #We are the first to transmit -> we have the token. We need to send a hello to everybody reachable.  
 def s1():
     global nodes
-    for node in nodes:
-      	responded, node["hasData"], node["hasToken"] = Functions.send_hello(myAddress, node["adress"])
-        if responded and not node["hasData"]:
-            node["toSendData"] = True
+    global nodesToSendToken
+    anyResponded = False
+    while not anyResponded:
+        for node in nodes:
+      	    responded, node["hasData"], node["hasToken"] = Functions.send_hello(myAddress, node["adress"])
+            if responded:
+                anyResponded = True
+                nodesToSendToken.append(node["adress"])
+            if responded and not node["hasData"]:
+                node["toSendData"] = True
+
     return s2()
 
 #Send data
@@ -62,49 +72,47 @@ def s2():
                 node["hasData"] = True
                 token += 1
                 lastNodeNoToken = node["adress"]
+            node["toSendData"] = False
+    
     return s3()
 
 #Updates token information and sends token to the last device that has received data. 
 #If we cannot send the token to the last one (has already had the token or it is unreachable), we need to try to send the token to another.
 def s3():
-    global nodesToSend
-    for node in nodes:
-        if node["toSendData"]:
-            nodesToSend.append(node["adress"])
+    responded = False
     if lastNodeNoToken > 0:
-        Functions.sendToken(lastNodeNoToken, token) # (Node adress, token)
-    else:
-        Functions.sendToken(random.choice(nodesToSend), token)
+        responded = Functions.sendToken(myAddress, lastNodeNoToken, token) # (Node adress, token)
+    while not responded:
+        responded = Functions.sendToken(myAddress, random.choice(nodesToSendToken), token)
     return s4()
 
 
 #State where we wait to reveive a packet
 def s4():
-    global rxData
-    packet_type, rxData = Functions.wait_read_packets() #TORNA EL VALOR HELLO_PACKET/DATA_PACKET/TOKEN_PACKET i DATA DEL PAQUET
-    if packet_type == CNTS.HELLO_PACKET:
+    global rcvData
+    packet_type, rcvData = Functions.wait_read_packets() #TORNA EL VALOR HELLO_PACKET/DATA_PACKET/TOKEN_PACKET i DATA DEL PAQUET
+    if packet_type == packets.HELLO["type"]:
         return s5()
-    elif packet_type == CNTS.DATA_PACKET:
+    elif packet_type == packets.DATA["type"]:
       	return s6() # Estat on guardes la data al fitxer
-    elif packet_type == CNTS.TOKEN_PACKET:
+    elif packet_type == packets.TOKEN["type"]:
       	return s7() # Estat on llegeixes el token
 
 def s5():
-  	Functions.send_hello_response(myAddress, haveData, hadToken)
-  	if not haveData:
-  	  	return s5()
+  	Functions.send_hello_response(myAddress, rcvData, haveData, hadToken)
+  	return s4()
 
 # XUCLAR DATA I GUARDAR EN FITXER
 def s6():
     global haveData
-    Functions.write_file(rxData) #into raspberry
+    Functions.write_file(rcvData) #into raspberry
     haveData = True
     return s4()
 
 #Update the information of the node with the information of the token    
 def s7():
     global token
-    token = Functions.read_token()
+    token = rcvData
     if token == 6:
         return s8()
     return s1()
