4691e4078eb069be1e5d1cb8ee3a964463f263c1
Miquellaneras <50173851+Miquellaneras@users.noreply.github.com>
2021-12-02 18:49:07 +0100

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -32,5 +32,5 @@ CHANNEL = 6
 ENCODING_TRANSMISSION = 'utf-8'
 PACKET_SIZE = 32
 DATA_SIZE = 30
-pipes_aux = [0x52, 0x78, 0x41, 0x41, 0x41]
-PIPES = bytearray(pipes_aux)
+PIPES_VECTOR = [0x52, 0x78, 0x41, 0x41, 0x41]
+PIPES = bytearray(PIPES_VECTOR)
